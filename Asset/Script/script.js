1d652918b1a43587f2559358121c7511539a72fa
JeffersonT.C <jeffersontovarcortez74@gmail.com>
2024-09-26 15:00:58 -0500

    Prueba

--- Asset/Script/script.js
+++ Asset/Script/script.js
@@ -35,7 +35,7 @@ function stopAutoplay() {
 }
 
 // Iniciar autoplay con un intervalo de 3 segundos.
-startAutoplay(3000);
+startAutoplay(10000);
 
 // Opcional: Detener autoplay cuando el usuario interactúa con los botones de navegación.
 document.querySelectorAll('.nav-button').forEach(button => {
